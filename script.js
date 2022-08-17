4b823a43b8ed1f15b5d91e09aba5f35414251c31
Greg Schuman <gcschuman@hotmail.com>
2022-08-17 12:26:46 -0700

    Initial commit: added notes of what I need to enter and the variable that lets the user select.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Variable that lets the user select
+const getUserChoice = userInput => {
+    userInput =>{
+        userInput = userInput.toLowerCase()
+        // If / else statement for user input
+        if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
+            return userInput
+        } else {
+            console.error
+        }
+    }
+}
+// Variable that lets the computer select
+// Variable that determines the winner
+// Variable that plays the game
